2c4a2fbe72005f5ccd7566de8425cee0d187a9d9
OleksandrKarpenkoUA <1ooksapp8@gmail.com>
2024-11-13 09:08:56 +0200

    test protected attributes

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-class Animal:
-    name = ""
-    MIN_COORD = 0
-    MAX_COORD = 100
-    def __init__(self, name):
-        self.name = name
+class Point:
+    def __init__(self, x=0, y=0):
+        self._x = x
+        self._y = y
 
-    @classmethod
-    def validate(cls, arg):
-        return cls.MIN_COORD <= arg <= cls.MAX_COORD
-    @classmethod
-    def test(cls):
-        print("Test")
-        return 0
-    def defaultmet(self, name):
-        print("Something")
-        return self.name
-
-    @staticmethod
-    def printName(name):
-        return name
-
-    def __del__(self):
-        print("Animal name is ")
-
-
-cat = Animal("Cher")
-print(cat.name)
-print(Animal.validate(18))
-print(Animal.test())
-print(Animal.printName("Sara"))
-cat.defaultmet("Name")
+pt = Point(1, 2)
+print(pt._x, pt._y)
